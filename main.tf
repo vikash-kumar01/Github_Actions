4fd7d5f0e6f5e97251b46a08243685c7679fa386
Mir Owahed Ali <133110800+mir-owahed@users.noreply.github.com>
2024-11-20 11:33:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,3 +71,16 @@ resource "aws_instance" "bastion" {
     Name = "bastion-host"
   }
 }
+
+resource "aws_security_group_rule" "example" {
+  security_group_id = aws_security_group.example.id
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+
+  lifecycle {
+    ignore_changes = [cidr_blocks]  # Ignore changes if you know they already exist
+  }
+}
