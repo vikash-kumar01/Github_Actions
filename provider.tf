2db82e873f110a117caf9cb70bdc624dab443847
Mir Owahed Ali <133110800+mir-owahed@users.noreply.github.com>
2024-11-20 11:24:22 +0530

    removed dynamodb table.

--- provider.tf
+++ provider.tf
@@ -9,7 +9,6 @@ terraform {
   backend "s3" {
     bucket         = "mir-terraform-s3-bucket"
     key            = "key/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "dynamodb-state-locking"    
+    region         = "ap-south-1"        
   }
 }
